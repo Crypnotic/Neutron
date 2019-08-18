e369296b943238f9402e48dd4d07224239cdb499
md678685 <1917406+md678685@users.noreply.github.com>
2019-08-18 20:07:55 +0100

    Fix inversion in assertNotIgnoring

--- src/main/java/me/crypnotic/neutron/api/command/CommandWrapper.java
+++ src/main/java/me/crypnotic/neutron/api/command/CommandWrapper.java
@@ -80,7 +80,7 @@ public abstract class CommandWrapper implements Command {
 
     @SneakyThrows
     public void assertNotIgnoring(CommandSource source, CommandSource ignoreSource, Player target, LocaleMessage message, Object... values) {
-        boolean ignoring = getUser(ignoreSource).map(user -> !user.isIgnoringPlayer(target)).orElse(false);
+        boolean ignoring = getUser(ignoreSource).map(user -> user.isIgnoringPlayer(target)).orElse(false);
         assertCustom(source, !ignoring, message, values);
     }
 
